6b395fc011cffdc63312b7258da3d7d9c07bae1b
s <109672385+Tuuturuturuu@users.noreply.github.com>
2022-09-27 12:23:24 +0200

    Delete Prácticas/pract4/Debug directory

--- Prácticas/pract4/Debug/lena128.d
+++
@@ -1,3 +0,0 @@
-lena128.d lena128.o: ../lena128.c ../lena128.h
-
-../lena128.h:
--- Prácticas/pract4/Debug/main.d
+++
@@ -1,7 +0,0 @@
-main.d main.o: ../main.c ../trafo.h ../types.h ../lena128.h
-
-../trafo.h:
-
-../types.h:
-
-../lena128.h:
